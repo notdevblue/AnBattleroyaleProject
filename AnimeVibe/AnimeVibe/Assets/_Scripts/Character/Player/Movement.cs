9ede61a4773f5f17a0840c04a048e09138f3cfee
HanAtNGLE <wooyeophan@gmail.com>
2023-02-02 15:22:48 +0900

    ㅁㄴㅇㄹ

---
+++ AnimeVibe/AnimeVibe/Assets/_Scripts/Character/Player/Movement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Character.Player
+{
+    public class Movement : MonoBehaviour
+    {
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+            
+        }
+    }
+}
